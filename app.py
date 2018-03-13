66a4af60d8f6d7e52b6a7fda476b73468d89424f
karingula <karingula@users.noreply.github.com>
2018-03-13 17:00:10 -0400

    path param type annotation

--- app.py
+++ app.py
@@ -5,19 +5,27 @@ from apistar import http
 
 
 
-def welcome(request: http.Request, query_params: http.QueryParams, user_agent: http.Headers):
-    return {
-    'method': request.method,
-    'url': request.url,
-    'headers': dict(request.headers),
-    'params': dict(query_params),
-    'user_agent': dict(user_agent),#here request.headers & http.Headers both are same
-
+def welcome(name: str, request: http.Request, query_params: http.QueryParams, user_agent: http.Headers, data: http.RequestData):
+    data = {"message": f'welcome to {name}'}
+    print(type(data))
+    # return{
+    # 'method': request.method,
+    #'url': request.url,
+    # 'headers': dict(request.headers),
+    # #'params': dict(query_params),
+    # #'user_agent': dict(user_agent),#here request.headers & http.Headers both are same
+    # 'data': data
+    # }
+    headers={'Location': '/greeting',
+             'url': request.url
     }
+
+    return http.Response(data, headers=headers, status=200)
+
 
 
 routes = [
-    Route('/', 'GET', welcome),
+    Route('/greeting/{name}', 'GET', welcome),
     Include('/docs', docs_urls),
     Include('/static', static_urls)
 ]
