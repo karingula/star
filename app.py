54bff37cc20eec5be0b545a0f7f7b834964f5622
karingula <karingula@users.noreply.github.com>
2018-03-14 17:58:56 -0400

    add http response - headers and status

--- app.py
+++ app.py
@@ -16,18 +16,22 @@ def get_player_details(player_name):
     return {'name': player_name}
 
 
-def get_all_players():
+def get_all_players(request: http.Request):
     players = get_players()
     player_list = [{'name': player, 'url': reverse_url(
         'example', player_name=player)} for player in players]
 
-    return {'players': player_list}
+    data = {'players': player_list}
+    # print(type(data))
+    # return data
+    headers = {'this_is_my_url':request.url}
+    return http.Response(data, headers=headers, status=200)
 
 
 routes = [
     Route(path='/players/', method='GET', view=get_all_players),
     Route(path='/players/{player_name}/', method='GET', view=get_player_details),
-    Route(path='/loli/{player_name}/', method='GET', view=example),
+    Route('/loli/{player_name}/', 'GET', example),
     Include('/docs', docs_urls),
 ]
 app = App(routes=routes)
