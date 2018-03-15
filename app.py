d87759ef84096ef6eb750cab01f1908dd80cd9fa
karingula <karingula@users.noreply.github.com>
2018-03-15 12:09:44 -0400

    templates & HTMLrenderer

--- app.py
+++ app.py
@@ -1,40 +1,68 @@
-from apistar import Include, Route, reverse_url
+from apistar import Include, Route, reverse_url, Response, http, typesystem, annotate, render_template
 from apistar.frameworks.wsgi import WSGIApp as App
 from apistar.handlers import docs_urls, static_urls
-from apistar import http
+from apistar.renderers import HTMLRenderer
 import typing
 
+
+class Rating(typesystem.Integer):
+    minimum = 1
+    maximum = 5
+
+
+class Sports(typesystem.Enum):
+    enum = ['cricket', 'golf', 'formula-1', 'horse_racing']
+
+
+class Player(typesystem.Object):
+    properties = {
+        'name': typesystem.string(max_length=100),
+        'rating': Rating,
+        'sports': Sports,
+        'retired': typesystem.boolean()
+    }
+
+
 def get_players():
-    player_list = ['apple','mango','banana','carrot','radish']
+    player_list = ['apple', 'mango', 'banana', 'carrot', 'radish']
 
     return player_list
+
 
 def example(player_name):
     return "this is just an example"
 
-def get_player_details(player_name):
-    return {'name': player_name}
+
+@annotate(renderers=[HTMLRenderer()])
+def get_player_details(player_name: str):
+    return render_template('index.html', player_name=player_name)
 
 
 def get_all_players(request: http.Request):
     players = get_players()
     player_list = [{'name': player, 'url': reverse_url(
-        'example', player_name=player)} for player in players]
+        'get_player_details', player_name=player)} for player in players]
 
     data = {'players': player_list}
     # print(type(data))
     # return data
-    headers = {'this_is_my_url':request.url}
-    return http.Response(data, headers=headers, status=200)
+    headers = {'this_is_my_url': request.url}
+    return Response(data, headers=headers, status=200)
 
 
 routes = [
     Route(path='/players/', method='GET', view=get_all_players),
-    Route(path='/players/{player_name}/', method='GET', view=get_player_details),
-    Route('/loli/{player_name}/', 'GET', example),
+    Route(path='/players/{player_name}/',
+          method='GET', view=get_player_details),
     Include('/docs', docs_urls),
 ]
-app = App(routes=routes)
+settings = {
+    'TEMPLATES': {
+        'ROOT_DIR': 'templates',
+        'PACKAGE_DIRS': ['apistar']  # Built-in apistar templates
+    }
+}
+app = App(routes=routes, settings=settings)
 
 if __name__ == '__main__':
     app.main()
