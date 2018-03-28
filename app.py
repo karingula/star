8937ad06d5896d7034c039d60e0e5b2605da8c87
karingula <karingula@users.noreply.github.com>
2018-03-28 17:56:33 -0400

    form data

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import service
 import models as Models
 from components import FlightComponent
 from db import session
+import pandas as pd
 
 
 class Rating(typesystem.Integer):
@@ -59,14 +60,23 @@ def get_all_players(request: http.Request):
     headers = {'this_is_my_url': request.url}
     return Response(data, headers=headers, status=200)
 
-def get_flight_details() -> typing.List[Models.Flight]:
+@annotate(renderers=[HTMLRenderer()])
+def get_flight_details(request: http.Request) -> typing.List[Models.Flight]:
+    #form=FlightForm()
+    print(request.method)
     data = [FlightComponent(instance)
             for instance in session.query(Models.Flight).all()]
-    return data
+    return render_template('flights.html', data=data)
 
-def add_flight(message: str):
+@annotate(renderers=[HTMLRenderer()])
+def add_flight(data:http.RequestData):
     print("Inside the add flight details")
-    return {'question': 'how are you?'}
+    print(type(data))
+    flight_data = data.to_dict(flat=False)
+    print(flight_data)
+    df = pd.DataFrame.from_dict(flight_data, orient='columns')
+    print(df)
+    return render_template('flights.html')
 
 routes = [
     Route('/players', 'GET', get_all_players),
@@ -82,60 +92,67 @@ settings = {
     }
 }
 
-#getting all primary keys
-pk_list = []
-pk_list = [x.key for x in inspect(Flight).primary_key]
-print("Normal Inspection:", pk_list)
 
-#  Ripping __table_args__ apart for clear understanding
-for z in Flight.__table_args__:
-    # Each item of __table_args__ is an UniqueConstraint object
-    # UniqueConstraint object inherits ColumnCollection object
-    # ColumnCollection object inherits util.OrderedProperties object
-    # OrderedProperties object inherits Properties object
-    print(type(z))
-    #__visit_name__, columns are properties of a Properties object
-    print(z.__visit_name__)
-    # columns is a ColumnCollection object which is iterable
-    print(type(z.columns))
-    for y in z.columns:
-        # Each element of ColumnCollection object is a Column object from schema
-        # ex: <class 'sqlalchemy.sql.schema.Column'>
-        print(type(y))
-        #name is the property of Column object which gives the name of the Column of SQLAlchemy model
-        print(y.name)
-    col_list = tuple([k.name for k in z.columns.__iter__()])
-    print("This is col list:",col_list)
 
-uc_cols = []
-pk_cols = []
-nul_cols = []
-#getting all the columns involved in UniqueConstraints and PrimaryKeyConstraint separately
-uc_cols, pk_cols = service.lisftify_columns(Flight)
-pk_cols+=pk_list
-#create a dataframe
-list1 = [1, ('Vanc','ouver', 'Canada'), 'Toronto', '3-Jan']
-list2 = [2, ('Amst','erdam', 'Netherlands'), None, '15-Feb']
-list3 = [4, None, 'Glasgow', '12-Jan']
-list4 = [9, ('Halm','stad', 'Norway'), 'Athens', '21-Jan']
-list5 = [3, ('Bris','bane', 'Australia'), 'Toronto', None]
-list6 = [4, ('Johan','nesburg', 'South Africa'), 'Venice', '12-Jan']
-list7 = [9, None, None, '20-Oct']
-data = [list1,list2,list3,list4,list5,list6,list7]
-df = pd.DataFrame(data, columns=['flight_id','from_location','to_location','schedule'])
 
-violation_checker = service.Violation()
 
-#get all the columns which violated the unique constraints
-uc_violations = list(map(functools.partial(violation_checker.fetch_violation_records_uniqueness, df=df), uc_cols))
-print("UC_Violations",uc_violations)
-# get all the columns which violated the primary key constraints
-pk_violations = list(map(functools.partial(violation_checker.fetch_violation_records_uniqueness, df=df), pk_cols))
-print("PK Violations:", pk_violations)
-nul_cols = [col.name for col in Flight.__table__.columns if not col.nullable]
-# print("Nullable Cols:",nul_cols)
-nul_violations = violation_checker.fetch_violation_records_nullable(df=df,cols=nul_cols)
-print("Nullable Violations:", nul_violations)
+
+
+#
+# #getting all primary keys
+# pk_list = []
+# pk_list = [x.key for x in inspect(Models.Flight).primary_key]
+# print("Normal Inspection:", pk_list)
+#
+# #  Ripping __table_args__ apart for clear understanding
+# for z in Models.Flight.__table_args__:
+#     # Each item of __table_args__ is an UniqueConstraint object
+#     # UniqueConstraint object inherits ColumnCollection object
+#     # ColumnCollection object inherits util.OrderedProperties object
+#     # OrderedProperties object inherits Properties object
+#     print(type(z))
+#     #__visit_name__, columns are properties of a Properties object
+#     print(z.__visit_name__)
+#     # columns is a ColumnCollection object which is iterable
+#     print(type(z.columns))
+#     for y in z.columns:
+#         # Each element of ColumnCollection object is a Column object from schema
+#         # ex: <class 'sqlalchemy.sql.schema.Column'>
+#         print(type(y))
+#         #name is the property of Column object which gives the name of the Column of SQLAlchemy model
+#         print(y.name)
+#     col_list = tuple([k.name for k in z.columns.__iter__()])
+#     print("This is col list:",col_list)
+#
+# uc_cols = []
+# pk_cols = []
+# nul_cols = []
+# #getting all the columns involved in UniqueConstraints and PrimaryKeyConstraint separately
+# uc_cols, pk_cols = service.lisftify_columns(Models.Flight)
+# pk_cols+=pk_list
+# #create a dataframe
+# list1 = [1, ('Vanc','ouver', 'Canada'), 'Toronto', '3-Jan']
+# list2 = [2, ('Amst','erdam', 'Netherlands'), None, '15-Feb']
+# list3 = [4, None, 'Glasgow', '12-Jan']
+# list4 = [9, ('Halm','stad', 'Norway'), 'Athens', '21-Jan']
+# list5 = [3, ('Bris','bane', 'Australia'), 'Toronto', None]
+# list6 = [4, ('Johan','nesburg', 'South Africa'), 'Venice', '12-Jan']
+# list7 = [9, None, None, '20-Oct']
+# data = [list1,list2,list3,list4,list5,list6,list7]
+# df = pd.DataFrame(data, columns=['flight_id','from_location','to_location','schedule'])
+#
+# violation_checker = service.Violation()
+#
+# #get all the columns which violated the unique constraints
+# uc_violations = list(map(functools.partial(violation_checker.fetch_violation_records_uniqueness, df=df), uc_cols))
+# print("UC_Violations",uc_violations)
+# # get all the columns which violated the primary key constraints
+# pk_violations = list(map(functools.partial(violation_checker.fetch_violation_records_uniqueness, df=df), pk_cols))
+# print("PK Violations:", pk_violations)
+# nul_cols = [col.name for col in Models.Flight.__table__.columns if not col.nullable]
+# # print("Nullable Cols:",nul_cols)
+# nul_violations = violation_checker.fetch_violation_records_nullable(df=df,cols=nul_cols)
+# print("Nullable Violations:", nul_violations)
 app = App(routes=routes, settings=settings)
 
 if __name__ == '__main__':
